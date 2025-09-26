6bdab9e350d5d0d54a725b4a5eca52e9bf0677c0
Masudur400 <md8004210@gmail.com>
2025-09-26 14:20:51 +0600

    google authenticaton add

---
+++ src/app/(public)/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from '@/components/modules/Blogs/auth/RegisterForm';
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div>
+             <RegisterForm></RegisterForm>
+        </div>
+    );
+};
+
+export default RegisterPage;
